354cd34fe665f2a0ec0722c766af0a263d81d940
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-19 03:54:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,8 @@ class Student
    INSERT INTO students (name, grade) 
     VALUES (?, ?)
    SQL
-   DB[:conn].execute(sql, self.name, self.album)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+   DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
   
 end
